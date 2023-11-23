60b3e0a80cef6d9548b0deeeeffd364ac1d312f6
Alfaza Satria <alfazasatria8@gmail.com>
2023-11-23 20:10:28 +0700

    :lollipop: feat : add city with filter

--- routes/fetch.js
+++ routes/fetch.js
@@ -2,43 +2,51 @@ var express = require('express');
 const fetch = require('node-fetch');
 var router = express.Router();
 
-const API_KEY ='2ca5b453b28dde9492447338ccffaea2';
+const API_KEY = '2ca5b453b28dde9492447338ccffaea2';
 
-async function fetchLocation(url, apiKey, options = {}){
-    if(apiKey){
+async function fetchLocation(url, apiKey, options = {}) {
+    if (apiKey) {
         options.headers = {
             ...options.headers,
-            'key' : apiKey
+            'key': apiKey
         };
     }
 
-    try{
-        const result = await fetch (url, options);
-        if(!result.ok){
+    try {
+        const result = await fetch(url, options);
+        if (!result.ok) {
             throw new Error(`Error : ${result.status}`);
         }
         return await result.json(result);
-    }catch(error){
+    } catch (error) {
         res.send('Fetch error', error);
         throw error;
     }
 }
 
 router.get('/province', async (req, res) => {
-    try{
+    try {
         const data = await fetchLocation('https://pro.rajaongkir.com/api/province', API_KEY);
         res.json(data);
-    }catch (error){
-        res.status(500).json({error : error.message});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 })
 
 router.get('/city', async (req, res) => {
-    try{
-        const data = await fetchLocation('https://pro.rajaongkir.com/api/city', API_KEY);
+    try {
+        const { id, province } = req.query;
+        let url = 'https://pro.rajaongkir.com/api/city';
+        const queryParams = new URLSearchParams();
+        if (id) queryParams.append('id', id);
+        if (province) queryParams.append('province', province);
+        if ([...queryParams].length) url += `?${queryParams}`;
+        
+        const data = await fetchLocation(url, API_KEY);
+
         res.json(data);
-    }catch (error){
-        res.status(500).json({error : error.message});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 })
 
