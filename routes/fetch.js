40c3b413ce6ce4c14f4fe91b2ed930c25a2237a6
Alfaza Satria <alfazasatria8@gmail.com>
2024-01-16 20:57:05 +0700

    :lollipop: feat : add save to db on 1 file

--- routes/fetch.js
+++ routes/fetch.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const fetch = require('node-fetch');
+const prisma = require('../db');
 var router = express.Router();
 
 const API_KEY = '2ca5b453b28dde9492447338ccffaea2';
@@ -21,6 +22,17 @@ async function fetchLocation(url, apiKey, options = {}) {
     } catch (error) {
         res.send('Fetch error', error);
         throw error;
+    }
+}
+
+async function saveProvincesToDb(provinces) {
+    for (const province of provinces) {
+        await prisma.province.create({
+            data: {
+                provinceId: province.province_id, 
+                name: province.province
+            }
+        });
     }
 }
 
@@ -66,5 +78,18 @@ router.get('/subdistrict', async (req,res) => {
         res.status(500).json({ error: error.message});
     }
 })
+
+router.get('/store/province', async (req, res) => {
+    try {
+        const apiResponse = await fetchLocation('https://pro.rajaongkir.com/api/province', API_KEY);
+        const provincesData = apiResponse.rajaongkir.results; 
+
+        await saveProvincesToDb(provincesData); 
+
+        res.json(provincesData); 
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 module.exports = router;
